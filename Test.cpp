2dcf8bea497700ef06973da5fe9cf6da78c0e2a1
BlackRaven237 <youmbincbovan@gmail.com>
2025-10-28 04:03:21 +0100

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char** argv){
+    
+    // chaine1[] = "Bonjour"
+    // chaine2[] = "Bonsoir"
+    // BonjourBonsoir
+
+    // bonjour 
+    // CompterOccurences("Bonjour", 'o') = 2
+    
+    return 0;
+}
